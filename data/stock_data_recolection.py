0a5b2cc1cbb13fda0c213c1c9714838c49ab2f05
Luis GutiérrezLlamedo <luismarkgo@gmail.com>
2024-10-02 21:03:52 +0200

    Changed Stock Datetime Recolection

--- data/stock_data_recolection.py
+++ data/stock_data_recolection.py
@@ -2,9 +2,10 @@ import yfinance
 
 tickets = ["MSFT", "GOOGL", "META", "AMZN", "NVDA", "IBM", "CRM"]
 
-start_date = "2021-01-01"
-end_date = "2024-01-01"
+end_date = "2021-01-01"
+start_date = "2011-01-01"
 
-for ticket in tickets:
+for i,ticket in enumerate(tickets):
+    print(f"{i+1}/{len(tickets)} Recolectando datos de {ticket}...")
     data = yfinance.download(ticket, start=start_date, end=end_date)
-    data.to_csv(f"data/{ticket}.csv")
+    data.to_csv(f"data/raw/{ticket}.csv")
