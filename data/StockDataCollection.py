8cf75901d570c2eb60fc67bd99aafc0bf364a7e1
Luis GutiérrezLlamedo <luismarkgo@gmail.com>
2024-10-03 19:03:15 +0200

    Added NASQAD stock data

--- data/StockDataCollection.py
+++ data/StockDataCollection.py
@@ -1,4 +1,5 @@
 import yfinance
+import pandas as pd
 
 # Get tickers from NASDAQ
 with open("data/nasdaq-listed-symbols_csv.csv") as f:
@@ -13,4 +14,13 @@ end_date = "2021-01-01"
 data = yfinance.download(tickets, start=start_date, end=end_date)
 data.columns = [f"{ticker}_{type}" for type, ticker in data.columns]
 data = data.reindex(sorted(data.columns), axis=1)
-data.to_csv("data.csv")
+
+# Set the number of parts
+n_parts = 4  # Change this value to divide into more or fewer parts
+
+# Split data into n parts and save
+n = len(data) // n_parts
+for i in range(n_parts):
+    start_index = i * n
+    end_index = (i + 1) * n if i < n_parts - 1 else len(data)
+    data.iloc[start_index:end_index].to_csv(f"data/raw/NASDAQ_stock_data_part{i + 1}.csv")
