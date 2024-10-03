fa26f1182ed1d0ad1048dd0a4f3822b207f15bf0
Luis GutiérrezLlamedo <luismarkgo@gmail.com>
2024-10-03 16:43:36 +0200

    Updated stock collection date range

--- data/stock_data_recolection.py
+++ data/stock_data_recolection.py
@@ -2,8 +2,8 @@ import yfinance
 
 tickets = ["MSFT", "GOOGL", "META", "AMZN", "NVDA", "IBM", "CRM"]
 
+start_date = "2012-01-01"
 end_date = "2021-01-01"
-start_date = "2011-01-01"
 
 for i,ticket in enumerate(tickets):
     print(f"{i+1}/{len(tickets)} Recolectando datos de {ticket}...")
